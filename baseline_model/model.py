ef044dc1f64ad57396ad80eb172b2f195ecc1f37
tre3iii <lilshed3@gmail.com>
2026-03-15 23:38:35 -0700

    tweak baseline model and have it use an example from one of our datasets

--- baseline_model/model.py
+++ baseline_model/model.py
@@ -8,45 +8,65 @@ Model was loaded via Hugging Face.
 
 """
 
+import datasets
 import torch
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    BartForConditionalGeneration,
+)
 
 #Set to CUDA gpu if available, else -> CPU
-device = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+# Constants
 MODEL_ID = "facebook/bart-large-cnn"
+DATASET_ID = "abisee/cnn_dailymail"
+DATASET_VERSION = "3.0.0"
+TEST_SPLIT = "test"
 
-# Load tokenizer and model
+test_split = datasets.load_dataset(DATASET_ID, DATASET_VERSION, split=TEST_SPLIT)
+
+print("\r\n")
+print("Dataset format: ")
+print(test_split)
+print("\r\n")
+
+print("Example Article: ")
+print("\r\n")
+print(test_split[1000]["article"])
+print("\r\n")
+
+test_article = test_split[1000]["article"]
+
+"""
+TOKENIZE(R)
+"""
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-model = AutoModelForSeq2SeqLM.from_pretrained(
+
+tokens = tokenizer(test_article, return_tensors="pt", max_length=512, truncation=True).to(DEVICE)
+tensor = torch.tensor(tokens["input_ids"].to(DEVICE))
+# print(tokens)
+
+"""
+MODEL
+
+Pretrained BART model.
+"""
+model = BartForConditionalGeneration.from_pretrained(
     MODEL_ID,
     device_map="auto",
     torch_dtype=torch.float32,
 )
 
-text = (
-    "The tower is 324 metres (1,063 ft) tall, about the same height as an "
-    "81-storey building, and the tallest structure in Paris. Its base is square, "
-    "measuring 125 metres (410 ft) on each side. During its construction, the "
-    "Eiffel Tower surpassed the Washington Monument to become the tallest "
-    "man-made structure in the world, a title it held for 41 years until the "
-    "Chrysler Building in New York City was finished in 1930. It was the first "
-    "structure to reach a height of 300 metres. Due to the addition of a "
-    "broadcasting aerial at the top of the tower in 1957, it is now taller than "
-    "the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the "
-    "Eiffel Tower is the second tallest free-standing structure in France after "
-    "the Millau Viaduct."
-)
-
-inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True).to(device)
-
 # ensure no gradients are calculated or applied (we are doing inference, not training)
-with torch.no_grad():
-    outputs = model.generate(**inputs)
+outputs = model.generate(tensor)
 
-# Decode and print
-response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(response)
 
-print(model.__class__.__name__)
-print(model.config.activation_function)
+# use the tokenizer to decode the generated output tokens into text
+for item in outputs:
+    res = tokenizer.decode(item)
+    print(res)
+#some debuggng stuff
+#print(model.__class__.__name__)
+#print(model.config.activation_function)
